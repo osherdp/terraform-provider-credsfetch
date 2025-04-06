47259485cf96e91764a2bca7c087f994991474ca
Osher De Paz <osher.depaz@island.io>
2025-04-06 13:21:56 +0300

    Fix accaptance tests

--- internal/provider/credentials_data_source_test.go
+++ internal/provider/credentials_data_source_test.go
@@ -7,24 +7,30 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
-func TestAccExampleDataSource(t *testing.T) {
+func TestAccCredentialsDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
+			// assert that the data source has some values for the sensitive attributes
 			{
-				Config: testAccExampleDataSourceConfig,
+				Config: testAccCredentialsDataSourceConfig,
 				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(
-						"data.scaffolding_example.test",
-						tfjsonpath.New("id"),
-						knownvalue.StringExact("example-id"),
+					statecheck.ExpectSensitiveValue(
+						"data.credsfetch_credentials.test",
+						tfjsonpath.New("access_key_id"),
+					),
+					statecheck.ExpectSensitiveValue(
+						"data.credsfetch_credentials.test",
+						tfjsonpath.New("secret_access_key"),
+					),
+					statecheck.ExpectSensitiveValue(
+						"data.credsfetch_credentials.test",
+						tfjsonpath.New("session_token"),
 					),
 				},
 			},
@@ -32,8 +38,6 @@ func TestAccExampleDataSource(t *testing.T) {
 	})
 }
 
-const testAccExampleDataSourceConfig = `
-data "scaffolding_example" "test" {
-  configurable_attribute = "example"
-}
+const testAccCredentialsDataSourceConfig = `
+data "credsfetch_credentials" "test" {}
 `
